4125a7cb2d82c40bf4a1f216dcd7c99449b1cc80
Ulrich Bryan <nguekeubryan450@gmail.com>
2025-10-06 12:06:12 +0100

    Structure initiale de operations.c (fonctions à implémenter)

---
+++ src/operations.c
@@ -0,0 +1,37 @@
+#include "operations.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ====================================
+// Fonctions de base
+// ====================================
+
+void appliquer_dominante(ImagePPM *img, char couleur, int valeur) {
+    // TODO: a implementer par membre 3
+}
+
+void convertir_en_gris(ImagePPM *img) {
+    //TODO: a implementer par membre 3
+}
+
+void creer_negatif(ImagePPM *img) {
+    // TODO: a implementer par membre 3
+}
+
+void afficher_taille(const ImagePPM *img) {
+    // TODO: a implementer par membre 3
+}
+
+// ====================================
+// Fonctions avancees
+// ====================================
+
+ImagePPM* decouper_image(const ImagePPM *img, int l1, int l2, int c1, int c2) {
+    // TODO: a implementer par membre 4
+    return NULL;
+}
+
+void appliquer_filtre_median(ImagePPM *img) {
+    // TODO: a implementer par membre 4
+}
